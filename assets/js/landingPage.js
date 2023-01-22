d2b691782090bbf169f32a2f2ff6b59182bf58ff
Alessandro Ontani <casualotn@gmail.com>
2023-01-22 23:50:49 +0100

    completed the darkmode

---
+++ assets/js/landingPage.js
@@ -0,0 +1,7 @@
+const toggleBtn = document.getElementById("toggle-btn");
+// Elementi in dark:
+
+toggleBtn.onclick = function () {
+  //   this.classList.toggle("toggle-btn--dark");
+  document.body.classList.toggle("dark-mode");
+};
